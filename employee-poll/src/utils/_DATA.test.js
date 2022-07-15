aaf84cd14aff71236ae33a732a008c6de60469bf
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-07-15 20:39:48 +0100

    Added 5 working unit tests

--- employee-poll/src/utils/_DATA.test.js
+++ employee-poll/src/utils/_DATA.test.js
@@ -1,5 +1,47 @@
-import { render } from "@testing-library/react";
+import { _saveQuestion, _saveQuestionAnswer } from "./_DATA";
 
-import { _saveQuestion } from "./_DATA";
+describe('_saveQuestion', () => {
+  it('will save a new question if all expected fields are populated', async () => {
+    const optionOneText = "Would you rather be a web developer";
+    const optionTwoText = "Would you rather be a data scientist";
+    const author = "tylermcginnis";
 
-describe('_saveQuestion', async () => {});
+    const question = {optionOneText, optionTwoText, author};
+
+    const responseExpected = {
+      author,
+      optionOne: {
+        votes: [],
+        text: optionOneText,
+      },
+      optionTwo: {
+        votes: [],
+        text: optionTwoText,
+      }
+    };
+
+    expect(await _saveQuestion(question)).toMatchObject(responseExpected);
+  });
+
+  it('will fail to save new question if incorrect data is passed', async () => {
+    const question = "Please provide optionOneText, optionTwoText, and author";
+    await expect(_saveQuestion({})).rejects.toMatch(question);
+  });
+});
+
+describe('_saveQuestionAnswer', () => {
+  it('will save answer', async () => {
+    const authedUser = "tylermcginnis";
+    const qid = "xj352vofupe1dqz9emx13r";
+    const answer = "optionOne";
+
+    const response = {authedUser, qid, answer};
+
+    expect(await _saveQuestionAnswer(response)).resolves.toBeTruthy;
+  });
+
+  it('will fail to save answer', async () => {
+    const response = "Please provide authedUser, qid, and answer";
+    await expect(_saveQuestionAnswer({})).rejects.toMatch(response);
+  });
+});
