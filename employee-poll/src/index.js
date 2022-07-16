243668e0100ddf8ca54ec92db668dc6ee1700060
lerderl <josepholukunle1107@gmail.com>
2022-07-16 17:42:01 +0100

    Created the store

--- employee-poll/src/index.js
+++ employee-poll/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore } from 'redux';
+
 import './index.css';
 import App from './App';
+import reducer from './reducers'; 
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = legacy_createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
